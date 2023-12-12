0216fce3dde744167541436dce55f2bbfac2e277
Olaf Klöcker <npmokl@gmx.com>
2023-12-12 09:59:03 +0100

    create Puppeteer browser object only once every 10 invocations for better performance

--- index.js
+++ index.js
@@ -12,23 +12,25 @@ const getPdfOptions = (query = {}) => {
   return { format, scale, printBackground, headless, id, keepimgurl };
 };
 
-const getFontProperty = async (page) => {
-  const selector = ".page";
-  try {
-    const font = await page.evaluate((selector) => {
-      const title = document.querySelector(selector);
-      return getComputedStyle(title).font;
-    }, selector);
-    return font;
-  } catch (err) {
-    console.error('Expecteded selector ".page" to get font propert');
-  }
-};
+const launchBrowser = async () =>
+  await puppeteer.launch({
+    args: [...chromium.args, "--font-render-hinting=medium"],
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(
+      process.env.AWS_EXECUTION_ENV
+        ? "/opt/nodejs/node_modules/@sparticuz/chromium/bin"
+        : undefined
+    ),
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  });
+
+let browserLaunchCounter = 0;
+let browser;
 
 export const handler = async (event) => {
   console.log("event:", event);
 
-  //  console.log('event.body:', event.body)
   if (!event?.body?.length) {
     console.log("Missing payload");
     return {
@@ -51,29 +53,21 @@ export const handler = async (event) => {
 
   console.log(
     "Got request with size %s and ID '%s' keepimgurl=%s",
-    payload.length,
+    payload?.length,
     id,
     keepimgurl,
     new Date()
   );
 
-  const browser = await puppeteer.launch({
-    args: [...chromium.args, "--font-render-hinting=medium"],
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath(
-      process.env.AWS_EXECUTION_ENV
-        ? "/opt/nodejs/node_modules/@sparticuz/chromium/bin"
-        : undefined
-    ),
-    headless: chromium.headless,
-    ignoreHTTPSErrors: true,
-  });
+  if (!browser || browserLaunchCounter % 10 === 0) {
+    if (browser) await browser.close();
+    browser = await launchBrowser();
+    browserLaunchCounter++;
+  }
 
   const page = await browser.newPage();
   await page.setContent(payload);
   await page.emulateMediaType("screen");
-  await page.evaluateHandle("document.fonts.ready");
-  console.log("fontProperty: ", await getFontProperty(page));
 
   const buffer = await page.pdf({
     format,
@@ -83,15 +77,8 @@ export const handler = async (event) => {
   });
 
   await page.close();
-  await browser.close();
 
   console.log("Returning base64 encoded PDF of length", buffer.length);
-
-  // const response = {
-  //   statusCode: 200,
-  //   type: "application/pdf",
-  //   body: buffer.toString("base64"),
-  // };
 
   return buffer.toString("base64");
 };
